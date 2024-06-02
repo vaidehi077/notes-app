ff2ce45bd7f0b1dc85a41c5f6e5b0eae789c3e69
vaidehi077 <vaishnavvaidehi03@gmail.com>
2024-06-02 08:16:20 +0530

    All error fixed

--- client/src/components/NoteCard/NoteCard.js
+++ client/src/components/NoteCard/NoteCard.js
@@ -1,46 +1,36 @@
-import React from 'react';
-import './NoteCard.css';
-import DeleteIcon from './delete-icon.png';
-import UpdateIcon from './update-icon.png';
-import axios from 'axios';
-import { toast } from 'react-hot-toast';
+import React from 'react'
+import './NoteCard.css'
+import DeleteIcon from "./delete-icon.png"
+import UpdateIcon from "./update-icon.png"
+import axios from 'axios'
+import toast from 'react-hot-toast'
 import { Link } from 'react-router-dom';
 
 function NoteCard({ _id, title, content, category, loadNotes }) {
 
   const deleteNote = async () => {
-    try {
-      const response = await axios.delete(`${process.env.REACT_APP_API_URL}/notes/${_id}`);
-      toast.success(response.data.message);
-      loadNotes();
-    } catch (error) {
-      console.error('Error deleting note:', error);
-      toast.error('Failed to delete the note.');
-    }
-  };
+    const response = await axios.delete(`${process.env.REACT_APP_API_URL}/notes/${_id}`)
+    toast.success(response.data.message)
+    loadNotes()
+  }
 
   return (
     <div className='note-card'>
       <h3 className='note-card-title'>{title}</h3>
       <p className='note-card-content'>{content}</p>
       <span className='note-card-category'>{category}</span>
-      <div className='note-card-actions'>
-        <Link to={`/update/${_id}`}>
-          <img
-            src={UpdateIcon}
-            alt='update-icon'
-            className='update-icon'
-          />
-        </Link>
-        <img
-          src={DeleteIcon}
-          alt='delete-icon'
-          className='delete-icon'
-          onClick={deleteNote}
-        />
-      </div>
+      <img src={DeleteIcon}
+        alt='delete-icon'
+        className='delete-icon'
+        onClick={deleteNote} />
+
+      <Link to={`/update/${_id}`}>
+        <img src={UpdateIcon}
+          alt='update-icon'
+          className='update-icon' />
+      </Link>
     </div>
-  );
+  )
 }
 
-export default NoteCard;
+export default NoteCard
--- client/src/views/Home/Home.js
+++ client/src/views/Home/Home.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import axios from 'axios';
 import './Home.css'
-import NoteCard from "./../../components/NoteCard/NoteCard"
+import NoteCard from '../../components/NoteCard/NoteCard';
 
 function Home() {
   const [notes, setNotes] = useState([]);
