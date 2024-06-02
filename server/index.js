1d434da4c78232267e22b8bfb0ebfe2f13626496
vaidehi077 <vaishnavvaidehi03@gmail.com>
2024-06-02 08:14:55 +0530

    Error fixed

--- server/index.js
+++ server/index.js
@@ -1,114 +1,127 @@
-import express from 'express';
 import cors from 'cors';
-import mongoose, {model, Schema} from 'mongoose';
 import dotenv from 'dotenv';
+import express from 'express';
+import mongoose from 'mongoose';
 dotenv.config();
+
+import Note from './models/Note.js';
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
 const connectDB = async () => {
-    await mongoose.connect(process.env.MONGODB_URL)
-    console.log("Database connected")
-
+  await mongoose.connect(process.env.MONGODB_URL)
+  console.log("Database connected")
 }
-
 connectDB();
+
 const PORT = process.env.PORT || 5000;
 
-const noteSchema = new Schema({
-    title: String,
-    content: String,
-    category: String
-})
-
-
-const Note = model("Note", noteSchema);
-
 app.get("/health", (req, res) => {
-    res.json({
-        status:true,
-        message: "Server is running",
-        data: null
-    })
+
+  res.json({
+    success: true,
+    message: "Server is running",
+    data: null
+  })
 });
 
-app.post("/notes", async(req, res)=>
-{
-    const {title, content, category} = req.body;
+app.post("/notes", async (req, res) => {
+  const { title, content, category } = req.body;
 
-    const newNote = await Note.create ({
-        "title" :title,
-        "content" :content,
-        "category" : category
+  if(!title){
+    return res.json({
+      success: false,
+      message: "Title is required",
+      data: null
     })
+  }
 
-
-    res.json({
-        success: true,
-        message: "Note added successfully",
-        data:newNote
+  if(!content){
+    return res.json({
+      success: false,
+      message: "Content is required",
+      data: null
     })
+  }
+
+  if(!category){
+    return res.json({
+      success: false,
+      message: "Category is required",
+      data: null
+    })
+  }
+
+  const newNote = await Note.create({
+    "title": title,
+    "content": content,
+    "category": category
+  })
+
+  res.json({
+    success: true,
+    message: "Note added successfully",
+    data: newNote
+  })
 })
 
-   
+app.get("/notes", async (req, res) => {
 
-app.get("/notes", async(req, res)=>{
+  const notes = await Note.find();
 
-    const notes = await Note.find();
-    res.json({
-        success: true,
-        message: "Notes fetched successfully",
-        data: []
-    })
+  res.json({
+    success: true,
+    message: "Notes fetched successfully",
+    data: notes
+  })
 })
 
-app.get("/notes/:id" , async(req,res) => {
-    const {id} = req.params;
+app.get("/notes/:id", async (req, res) => {
+  const { id } = req.params;
 
-    const note = await Note.findOne({
-        _id: id
-    })
+  const note = await Note.findById(id);
 
-    res.json({
-        success: true,
-        message: "Note fetched successfully",
-        data: note
-    })
+  res.json({
+    success: true,
+    message: "Note fetched successfully",
+    data: note
+  })
 })
 
+app.put("/notes/:id", async (req, res) => {
+  const { id } = req.params;
 
-app.put("/notes/:id", async(req,res ) => {
-    const {id} = req.params;
+  const { title, content, category } = req.body;
 
-    const {title, content, category} = req.body;
+  await Note.updateOne({ _id: id }, {
+    $set: {
+      title: title,
+      content: content,
+      category: category
+    }
+  })
 
-    await Note.updateOne({_id: id}, {$set: {
-        title: title,
-        content: content,
-        category: category
-    }})
-    res.json({
-        success: true,
-        message: "Note updated successfully",
-        data: null
-    })
+  res.json({
+    success: true,
+    message: "Note updated successfully",
+    data: null
+  })
 })
 
-app.delete("/notes/:id", async(req,res ) => {
-    const {id} = req.params;
+app.delete("/notes/:id", async (req, res) => {
+  const { id } = req.params;
 
-    const {title, content, category} = req.body;
+  await Note.deleteOne({ _id: id })
 
-    await Note.deleteOne({_id: id})
-
-    res.json({
-        success: true,
-        message: "Note deleted successfully",
-        data: null
-    })
+  res.json({
+    success: true,
+    message: "Note deleted successfully",
+    data: null
+  })
 })
+
 app.listen(PORT, () => {
-    console.log(`Server is running at port ${PORT}`);
+  console.log(`Server is running on port ${PORT}`);
 });
